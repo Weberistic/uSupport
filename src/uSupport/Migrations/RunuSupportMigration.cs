92d4d8e9ebf75c8df940a156b4ab74a6c90e325e
Lantzify <lithvia@gmail.com>
2023-07-01 16:19:07 +0200

    Started adding new migration for add section to admin group

--- src/uSupport/Migrations/RunuSupportMigration.cs
+++ src/uSupport/Migrations/RunuSupportMigration.cs
@@ -1,11 +1,13 @@
 ﻿#if NETCOREAPP
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Migrations;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Infrastructure.Scoping;
 using Umbraco.Cms.Infrastructure.Migrations.Upgrade;
+
 
 namespace uSupport.Migrations
 {
@@ -16,12 +18,17 @@ namespace uSupport.Migrations
 		private readonly IKeyValueService _keyValueService;
 		private readonly IMigrationPlanExecutor _migrationPlanExecutor;
 
-		public RunuSupportMigration(IRuntimeState runtimeState, IScopeProvider scopeProvider, IMigrationPlanExecutor migrationPlanExecutor, IKeyValueService keyValueService)
+		public RunuSupportMigration(IUserService userService,
+			IRuntimeState runtimeState,
+			IScopeProvider scopeProvider,
+			IKeyValueService keyValueService,
+			IUmbracoContextFactory umbracoContext,
+			IMigrationPlanExecutor migrationPlanExecutor)
 		{
-			_migrationPlanExecutor = migrationPlanExecutor;
+			_runtimeState = runtimeState;
 			_scopeProvider = scopeProvider;
 			_keyValueService = keyValueService;
-			_runtimeState = runtimeState;
+			_migrationPlanExecutor = migrationPlanExecutor;
 		}
 
 		public void Handle(UmbracoApplicationStartingNotification notification)
