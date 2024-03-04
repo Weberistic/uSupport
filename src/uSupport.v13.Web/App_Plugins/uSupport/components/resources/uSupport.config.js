d841c1997ea0ad2721a8ff42fd9028c31a3b65f3
Johannes Lantz <lithvia@gmail.com>
2024-03-04 15:05:34 +0100

    Removed unwanted App_plugins

--- src/uSupport.v13.Web/App_Plugins/uSupport/components/resources/uSupport.config.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-    angular.module("umbraco").constant("uSupportConfig", {
-        "baseApiUrl": "/umbraco/backoffice/api/",
-        "basePathAppPlugins": "/App_Plugins/uSupport/"
-    });
-})();
